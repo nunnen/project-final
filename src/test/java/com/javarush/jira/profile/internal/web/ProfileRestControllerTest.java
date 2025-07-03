850e7a7ef1c697e1fce17e960bd32418da69c52a
Niyaz <niazsajdullin@gmail.com>
2025-07-03 15:07:55 +0500

    add link to secrets file to application.yaml

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,32 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 class ProfileRestControllerTest extends AbstractControllerTest {
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    @DisplayName("Get profile with authorized user")
+    void getProfileSuccess() {
+        fail("Not implemented");
+    }
+
+    @Test
+    @DisplayName("Get profile with unauthorized user")
+    void getProfileFail() {
+        fail("Not implemented");
+    }
+
+    @Test
+    void update() {
+    }
 }
